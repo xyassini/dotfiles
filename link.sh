10782e509b0218979c9ee3cac1eb177ea570f49a
Yassine Zeriouh <yassine.zeriouh@limehome.de>
2023-08-17 11:18:21 +0200

    Fix link script

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s "$(pwd)/fish" "$HOME/.config/"
-ln -s "$(pwd)/nvim" "$HOME/.config/"
+ln -s "$(pwd)/fish" "$HOME/.config"
+ln -s "$(pwd)/nvim" "$HOME/.config"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -s "$(pwd)/iterm2/com.googlecode.iterm2.plist" "$HOME/.config/iterm2/com.googlecode.iterm2.plist"
