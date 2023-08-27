6ca3cf6c8a27d3ba9ca5a40e8048cad31d8742b0
Yassine Zeriouh <yassine@zeriouh.io>
2023-08-27 14:59:53 +0200

    Start i3 config

--- link.sh
+++ link.sh
@@ -6,3 +6,9 @@ ln -s "$(pwd)/nvim" "$HOME/.config"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   ln -s "$(pwd)/iterm2/com.googlecode.iterm2.plist" "$HOME/.config/iterm2/com.googlecode.iterm2.plist"
 fi
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+  ln -s "$(pwd)/i3" "$HOME/.config"
+fi
+
+
