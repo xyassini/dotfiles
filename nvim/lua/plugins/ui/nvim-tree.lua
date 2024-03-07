5859d28e9957d2fe9eb0029837b438cbbced45c6
Yassine Zeriouh <y@zeriouh.io>
2024-03-07 20:17:36 +0100

    Adjust nvim-tree configuration

--- nvim/lua/plugins/ui/nvim-tree.lua
+++ nvim/lua/plugins/ui/nvim-tree.lua
@@ -1,8 +1,13 @@
-local status, tree = pcall(require, "nvim-tree")
-if not status then
-  print("Error: nvim-tree not installed")
-  return
-end
-
-tree.setup({})
+require("nvim-tree").setup({
+  respect_buf_cwd = true,
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+})
 
