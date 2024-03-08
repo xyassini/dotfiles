6d1537a62b28e49646f36208848bc42ada82a332
Yassine Zeriouh <y@zeriouh.io>
2024-03-09 00:13:15 +0100

    Add svelte and astro support for debugprint

--- nvim/lua/plugins/lsp/debugprint.lua
+++ nvim/lua/plugins/lsp/debugprint.lua
@@ -1,6 +1,18 @@
+local js = {
+  left = "console.warn(\"",
+  right = "\")",
+  mid_var = "\", ",
+  right_var = ")"
+}
+
+
 require("debugprint").setup({
   create_keymaps = false,
   create_commands = false,
+  filetypes = {
+    [ "svelte" ] = js,
+    [ "astro" ] = js
+  }
 })
 
 vim.keymap.set("n", "<Leader>l", function()
