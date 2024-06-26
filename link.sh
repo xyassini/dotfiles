3a1430208004547f4ea6c142d9cdd2d3a3abc37c
Yassine Zeriouh <yassine.zeriouh@limehome.de>
2024-06-26 19:24:20 +0200

    Add zed configuration

--- link.sh
+++ link.sh
@@ -10,6 +10,11 @@ mkdir -p "$HOME/.config/nvim_bak"
 mv "$HOME/.config/nvim" "$HOME/.config/nvim_bak"
 ln -s "$(pwd)/nvim" "$HOME/.config"
 
+# Zed
+mkdir -p "$HOME/.config/zed_bak"
+mv "$HOME/.config/zed" "$HOME/.config/zed_bak"
+ln -s "$(pwd)/zed" "$HOME/.config"
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # iTerm2
   ln -s "$(pwd)/iterm2/com.googlecode.iterm2.plist" "$HOME/.config/iterm2/com.googlecode.iterm2.plist"
