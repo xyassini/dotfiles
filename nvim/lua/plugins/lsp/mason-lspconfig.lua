2671b3ac8982d26a9e24bc156d36e1305a63ecc0
Yassine Zeriouh <y@zeriouh.io>
2024-06-28 01:30:23 +0200

    Add rubocop support for nvim

--- nvim/lua/plugins/lsp/mason-lspconfig.lua
+++ nvim/lua/plugins/lsp/mason-lspconfig.lua
@@ -1,4 +1,4 @@
 require("mason-lspconfig").setup({
-  ensure_installed = {"lua_ls", "emmet_ls", "graphql", "html", "angularls", "dockerls", "svelte", "astro", "prettierd", "stylua", "rubocop"},
+  ensure_installed = {"lua_ls", "emmet_ls", "graphql", "html", "angularls", "dockerls", "svelte", "astro"},
   automatic_installation = true
 })
--- nvim/lua/plugins/lsp/none-ls.lua
+++ nvim/lua/plugins/lsp/none-ls.lua
@@ -3,9 +3,11 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.diagnostics.erb_lint,
+    null_ls.builtins.diagnostics.rubocop,
     null_ls.builtins.diagnostics.fish,
 
 
-    null_ls.builtins.formatting.erb_lint
+    null_ls.builtins.formatting.erb_lint,
+    null_ls.builtins.formatting.rubocop
   }
 })
