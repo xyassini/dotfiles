ba70cfe05c8949b0bb45a4ef8972f65af11f0ba0
Yassine Zeriouh <y@zeriouh.io>
2024-07-19 18:54:48 +0200

    Add helix and gitui dotfiles

--- link.sh
+++ link.sh
@@ -10,6 +10,16 @@ mkdir -p "$HOME/.config/nvim_bak"
 mv "$HOME/.config/nvim" "$HOME/.config/nvim_bak"
 ln -s "$(pwd)/nvim" "$HOME/.config"
 
+# Helix
+mkdir -p "$HOME/.config/helix_bak"
+mv "$HOME/.config/helix" "$HOME/.config/helix_bak"
+ln -s "$(pwd)/helix" "$HOME/.config"
+
+# gitui 
+mkdir -p "$HOME/.config/gitui_bak"
+mv "$HOME/.config/gitui" "$HOME/.config/gitui_bak"
+ln -s "$(pwd)/gitui" "$HOME/.config"
+
 # Zed
 mkdir -p "$HOME/.config/zed_bak"
 mv "$HOME/.config/zed" "$HOME/.config/zed_bak"
