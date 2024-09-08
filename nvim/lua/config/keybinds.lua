f8e2c2c08f2e40ff9de1d1f4504c50ba04df267d
Yassine Zeriouh <y@zeriouh.io>
2024-09-08 20:08:46 +0200

    Add keybind to toggle copilot

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,4 +1,4 @@
-local wk_status, which_key = pcall(require, 'which-key')
+local wk_status, which_key = pcall(require, "which-key")
 if not wk_status then
   return
 end
@@ -8,44 +8,104 @@ which_key.add({
     mode = { "n" },
 
     -- Pane switching
-    { "+",         "<C-w>l",                                                    desc = "Go to right pane" },
-    { "ü",         "<C-w>h",                                                    desc = "Go to left pane" },
-    { "ä",         "<C-w>j",                                                    desc = "Go to below pane" },
-    { "ß",         "<C-w>k",                                                    desc = "Go to above pane" },
+    { "+", "<C-w>l", desc = "Go to right pane" },
+    { "ü", "<C-w>h", desc = "Go to left pane" },
+    { "ä", "<C-w>j", desc = "Go to below pane" },
+    { "ß", "<C-w>k", desc = "Go to above pane" },
 
     -- Pane splitting
-    { "<C-+>",     "<cmd>:vs<CR><C-w>l<cmd>Telescope find_files<CR>",           desc = "Split pane right and open file picker" },
-    { "<C-ü>",     "<cmd>:vs<CR><C-w>h<cmd>Telescope find_files<CR>",           desc = "Split pane left and open file picker" },
-    { "<C-ä>",     "<cmd>:sp<CR><C-w>j<cmd>Telescope find_files<CR>",           desc = "Split pane below and open file picker" },
-    { "<C-ß>",     "<cmd>:sp<CR><C-w>k<cmd>Telescope find_files<CR>",           desc = "Split pane above and open file picker" },
+    {
+      "<C-+>",
+      "<cmd>:vs<CR><C-w>l<cmd>Telescope find_files<CR>",
+      desc = "Split pane right and open file picker",
+    },
+    {
+      "<C-ü>",
+      "<cmd>:vs<CR><C-w>h<cmd>Telescope find_files<CR>",
+      desc = "Split pane left and open file picker",
+    },
+    {
+      "<C-ä>",
+      "<cmd>:sp<CR><C-w>j<cmd>Telescope find_files<CR>",
+      desc = "Split pane below and open file picker",
+    },
+    {
+      "<C-ß>",
+      "<cmd>:sp<CR><C-w>k<cmd>Telescope find_files<CR>",
+      desc = "Split pane above and open file picker",
+    },
 
     -- Navigation
-    { "<C-p>",     "<cmd>Telescope find_files<CR>",                             desc = "Open file picker" },
-    { "<C-f>",     "<cmd>Telescope live_grep<CR>",                              desc = "Search in all files" },
-    { "<C-b>",     "<cmd>Telescope buffers<CR>",                                desc = "Open buffer file picker" },
-    { "<C-g>",     "<cmd>Telescope git_status<CR>",                             desc = "Open staged file picker" },
-    { "<leader>e", "<cmd>Neotree toggle<CR>",                                   desc = "Toggle Neotree" },
+    { "<C-p>", "<cmd>Telescope find_files<CR>", desc = "Open file picker" },
+    { "<C-f>", "<cmd>Telescope live_grep<CR>", desc = "Search in all files" },
+    { "<C-b>", "<cmd>Telescope buffers<CR>", desc = "Open buffer file picker" },
+    { "<C-g>", "<cmd>Telescope git_status<CR>", desc = "Open staged file picker" },
+    { "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neotree" },
 
     -- Other.nvim
-    { "<leader>o", "<cmd>Other<CR>",                                            desc = "Open related file" },
+    { "<leader>o", "<cmd>Other<CR>", desc = "Open related file" },
 
     -- Text manipulation
-    { "<leader>r", ":%s//g<left><left>",                                        desc = "Regex search and replace in file" },
-
+    {
+      "<leader>r",
+      ":%s//g<left><left>",
+      desc = "Regex search and replace in file",
+    },
 
     -- Productivity
-    { "<leader>?", function() require("which-key").show({ global = true }) end, desc = "Show keymaps" },
-    { "<leader>d", "<cmd>Neogen<CR>",                                           desc = "Generate annotation" },
-    { "<leader>g", "<cmd>LazyGit<CR>",                                          desc = "Open Lazygit" },
-    { "<leader>l", "<cmd>Logsitter<CR>",                                        desc = "Add logging statement" },
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = true })
+      end,
+      desc = "Show keymaps",
+    },
+    { "<leader>d", "<cmd>Neogen<CR>", desc = "Generate annotation" },
+    { "<leader>g", "<cmd>LazyGit<CR>", desc = "Open Lazygit" },
+    { "<leader>l", "<cmd>Logsitter<CR>", desc = "Add logging statement" },
+    { "<leader>c", "<cmd>Copilot toggle<CR>", desc = "Toggle GitHub Copilot" },
 
     -- Flash.nvim / Search label navigation
-    { "s",         function() require("flash").jump() end,                      desc = "Flash",                                mode = { "n", "x", "o" } },
-    { "S",         function() require("flash").treesitter() end,                desc = "Flash Treesitter",                     mode = { "n", "x", "o" } },
-    { "r",         function() require("flash").remote() end,                    desc = "Remote Flash",                         mode = "o" },
-    { "R",         function() require("flash").treesitter_search() end,         desc = "Treesitter Search",                    mode = { "o", "x" } },
-    { "<c-s>",     function() require("flash").toggle() end,                    desc = "Toggle Flash Search",                  mode = { "c" } },
-
+    {
+      "s",
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+      mode = { "n", "x", "o" },
+    },
+    {
+      "S",
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+      mode = { "n", "x", "o" },
+    },
+    {
+      "r",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+      mode = "o",
+    },
+    {
+      "R",
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Treesitter Search",
+      mode = { "o", "x" },
+    },
+    {
+      "<c-s>",
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+      mode = { "c" },
+    },
 
     -- Testing
     {
@@ -54,7 +114,7 @@ which_key.add({
         require("neotest").run.run()
       end,
       desc = "Run all tests",
-      group = "Neotest"
+      group = "Neotest",
     },
     {
       "tf",
@@ -62,7 +122,7 @@ which_key.add({
         require("neotest").run.run(vim.fn.expand("%"))
       end,
       desc = "Run tests in current file",
-      group = "Neotest"
+      group = "Neotest",
     },
     {
       "tS",
@@ -70,7 +130,7 @@ which_key.add({
         require("neotest").run.stop()
       end,
       desc = "Stop running tests",
-      group = "Neotest"
+      group = "Neotest",
     },
     {
       "tw",
@@ -78,7 +138,7 @@ which_key.add({
         require("neotest").watch.toggle()
       end,
       desc = "Toggle test watch mode",
-      group = "Neotest"
+      group = "Neotest",
     },
     {
       "ts",
@@ -86,10 +146,8 @@ which_key.add({
         require("neotest").summary.toggle()
       end,
       desc = "Toggle test summary",
-      group = "Neotest"
+      group = "Neotest",
     },
-
-
 
     -- LSP
     { "<leader>i", "<cmd>TSToolsAddMissingImports<CR>", desc = "Add missing imports" },
@@ -99,10 +157,10 @@ which_key.add({
         require("conform").format({
           lsp_fallback = true,
           async = false,
-          timeout_ms = 5000
+          timeout_ms = 5000,
         })
       end,
-      desc = "Format current buffer"
+      desc = "Format current buffer",
     },
 
     {
@@ -110,49 +168,49 @@ which_key.add({
       function()
         vim.lsp.buf.declaration()
       end,
-      desc = "Go to declaration"
+      desc = "Go to declaration",
     },
     {
       "gd",
       function()
         vim.lsp.buf.definition()
       end,
-      desc = "Go to definition"
+      desc = "Go to definition",
     },
     {
       "K",
       function()
         vim.lsp.buf.hover()
       end,
-      desc = "Show documentation for symbol under cursor"
+      desc = "Show documentation for symbol under cursor",
     },
     {
       "gi",
       function()
         vim.lsp.buf.implementation()
       end,
-      desc = "Go to implementation"
+      desc = "Go to implementation",
     },
     {
       "<C-k>",
       function()
         vim.lsp.buf.signature_help()
       end,
-      desc = "Show signature help"
+      desc = "Show signature help",
     },
     {
       "<leader>D",
       function()
         vim.lsp.buf.type_definition()
       end,
-      desc = "Go to type definition"
+      desc = "Go to type definition",
     },
     {
       "<leader>n",
       function()
         vim.lsp.buf.rename()
       end,
-      desc = "Rename symbol under cursor"
+      desc = "Rename symbol under cursor",
     },
     {
       "<C-Enter>",
@@ -160,50 +218,19 @@ which_key.add({
         vim.lsp.buf.code_action()
       end,
       desc = "Show code actions",
-      mode = { "n", "v" }
+      mode = { "n", "v" },
     },
     {
       "gr",
       function()
         vim.lsp.buf.references()
       end,
-      desc = "Show references to symbol under cursor"
-    }
+      desc = "Show references to symbol under cursor",
+    },
   },
   {
     mode = { "v" },
 
     { "<leader>l", "<cmd>LogsitterV<CR>", desc = "Add logging statement" },
-  }
+  },
 })
-
---[[
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<C-Enter>', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
-})
---]]
