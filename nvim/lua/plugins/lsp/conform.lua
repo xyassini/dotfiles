28c4ba85f20a1ed007c08ddccdfe86588aeb8545
Yassine Zeriouh <y@zeriouh.io>
2024-09-08 16:23:14 +0200

    Fix conform configuration

--- nvim/lua/plugins/lsp/conform.lua
+++ nvim/lua/plugins/lsp/conform.lua
@@ -4,16 +4,16 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      python = { "isort", "black" },
+      python = { "isort", "black", stop_after_first = true },
       ruby = { "rubocop" },
       eruby = { "rubocop" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      css = { { "prettierd", "prettier" } },
-      html = { { "prettierd", "prettier" } },
-      mjml = { { "prettierd", "prettier" } },
-      svelte = { { "prettierd", "prettier" } },
-      astro = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "pretier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+      mjml = { "prettierd", "prettier", stop_after_first = true },
+      svelte = { "prettierd", "prettier", stop_after_first = true },
+      astro = { "prettierd", "prettier", stop_after_first = true },
     },
   }
 }
