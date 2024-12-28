09df8964cf8da4d2d04ca5177b77a62dc31085b7
Yassine Zeriouh <y@zeriouh.io>
2024-12-28 14:38:45 +0100

    LSP improvements

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -3,6 +3,8 @@ require("mason-lspconfig").setup()
 
 require("lsp.diagnostics")
 
+local util = require("lspconfig.util")
+
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason-lspconfig").setup_handlers({
@@ -10,11 +12,6 @@ require("mason-lspconfig").setup_handlers({
     require("lspconfig")[server_name].setup({
       capabilities = capabilities,
     })
-  end,
-
-  ["tsserver"] = function()
-    -- Deprecated
-    -- Skip since we use typescript-tools.nvim
   end,
 
   ["ts_ls"] = function()
@@ -29,14 +26,14 @@ require("mason-lspconfig").setup_handlers({
   ["angularls"] = function()
     require("lspconfig").angularls.setup({
       capabilities = capabilities,
-      root_dir = require("lspconfig/util").root_pattern("angular.json", "project.json"),
+      root_dir = util.root_pattern("angular.json", "project.json"),
     })
   end,
 
   ["tailwindcss"] = function()
     require("lspconfig").tailwindcss.setup({
       capabilities = capabilities,
-      root_dir = require("lspconfig/util").root_pattern("tailwind.config.{js,ts,mjs,cjs}"),
+      root_dir = util.root_pattern("tailwind.config.{js,ts,mjs,cjs}"),
     })
   end,
 
@@ -59,10 +56,17 @@ require("mason-lspconfig").setup_handlers({
 
 require("lspconfig").sourcekit.setup({
   capabilities = capabilities,
+  filetypes = { "swift" },
+  cmd = { vim.trim(vim.fn.system("xcrun -f sourcekit-lsp")) },
   on_init = function(client)
     -- HACK: to fix some issues with LSP
     -- more details: https://github.com/neovim/neovim/issues/19237#issuecomment-2237037154
     client.offset_encoding = "utf-8"
   end,
+  root_dir = function(filename, _)
+    return util.root_pattern("buildServer.json")(filename)
+      or util.root_pattern("*.xcodeproj", "*.xcworkspace")(filename)
+      or util.find_git_ancestor(filename)
+      or util.root_pattern("Package.swift")(filename)
+  end,
 })
-
