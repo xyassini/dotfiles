59c91d4b1e9140debfa0808f95b55788f1bbd7c2
Yassine Zeriouh <y@zeriouh.io>
2025-07-20 15:31:26 +0200

    Fix window picker for Neotree

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
   'stevearc/conform.nvim',
-  event = "LspAttach",
+  event = 'LspAttach',
   config = function()
     require('conform').setup {
       formatters_by_ft = {
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,55 +1,55 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  cmd = "Neotree",
-  branch = "v3.x",
+  'nvim-neo-tree/neo-tree.nvim',
+  cmd = 'Neotree',
+  branch = 'v3.x',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
     {
-      "s1n7ax/nvim-window-picker",
-      version = "2.*",
-      cmd = "Neotree",
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      event = "VeryLazy",
       config = function()
-        require("window-picker").setup({
+        require('window-picker').setup {
           hint = 'statusline-winbar',
           selection_chars = 'ABCDEFGHIJKLMNOP',
           filter_rules = {
             include_current_win = false,
             autoselect_one = true,
             bo = {
-              filetype = { "neo-tree", "neo-tree-popup", "notify" },
-              buftype = { "terminal", "quickfix" },
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              buftype = { 'terminal', 'quickfix' },
             },
           },
-        })
+        }
       end,
-    }
+    },
   },
   config = function()
-    require("neo-tree").setup({
+    require('neo-tree').setup {
       auto_clean_after_session_restore = true,
-      popup_border_style = "",
+      popup_border_style = '',
       enable_git_status = true,
       enable_diagnostics = true,
       window = {
         mappings = {
-          ["<CR>"] = "open_with_window_picker"
-        }
+          ['<CR>'] = 'open_with_window_picker',
+        },
       },
       buffers = {
         follow_current_file = {
           enabled = true,
-          leave_dirs_open = true
-        }
+          leave_dirs_open = true,
+        },
       },
       filesystem = {
-        hijack_netrw_behavior = "disabled",
+        hijack_netrw_behavior = 'disabled',
         follow_current_file = {
           enabled = true,
           leave_dirs_open = true,
         },
-      }
-    })
-  end
+      },
+    }
+  end,
 }
