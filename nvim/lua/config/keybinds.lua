a96d3d8d21c36c02c33ec503e14f66e68b15dfaa
Yassine Zeriouh <y@zeriouh.io>
2025-07-20 15:29:40 +0200

    Fix conform config

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -1,165 +1,162 @@
-local wk_status, which_key = pcall(require, "which-key")
+local wk_status, which_key = pcall(require, 'which-key')
 if not wk_status then
   return
 end
 
-which_key.add({
+which_key.add {
   -- Pane switching
   {
-    "+",
+    '+',
     function()
-      vim.cmd("wincmd l")
+      vim.cmd 'wincmd l'
     end,
-    desc = "Go to right pane"
+    desc = 'Go to right pane',
   },
   {
-    "ü",
+    'ü',
     function()
-      vim.cmd("wincmd h")
+      vim.cmd 'wincmd h'
     end,
-    desc = "Go to left pane"
+    desc = 'Go to left pane',
   },
   {
-    "ä",
+    'ä',
     function()
-      vim.cmd("wincmd j")
+      vim.cmd 'wincmd j'
     end,
-    desc = "Go to below pane"
+    desc = 'Go to below pane',
   },
   {
-    "ß",
+    'ß',
     function()
-      vim.cmd("wincmd k")
+      vim.cmd 'wincmd k'
     end,
-    desc = "Go to above pane"
+    desc = 'Go to above pane',
   },
 
   -- Pane splitting
   {
-    "<C-+>",
+    '<C-+>',
     function()
       vim.cmd.vs()
-      vim.cmd("wincmd l")
+      vim.cmd 'wincmd l'
       require('fzf-lua').files()
     end,
-    desc = "Split pane right and open file picker",
+    desc = 'Split pane right and open file picker',
   },
   {
-    "<C-ü>",
+    '<C-ü>',
     function()
       vim.cmd.vs()
       require('fzf-lua').files()
     end,
-    desc = "Split pane left and open file picker",
+    desc = 'Split pane left and open file picker',
   },
   {
-    "<C-ä>",
+    '<C-ä>',
     function()
       vim.cmd.sp()
-      vim.cmd("wincmd j")
+      vim.cmd 'wincmd j'
       require('fzf-lua').files()
     end,
-    desc = "Split pane below and open file picker",
+    desc = 'Split pane below and open file picker',
   },
   {
-    "<C-ß>",
+    '<C-ß>',
     function()
       vim.cmd.sp()
       require('fzf-lua').files()
     end,
-    desc = "Split pane above and open file picker",
+    desc = 'Split pane above and open file picker',
   },
 
   -- Pickers
   {
-    "<C-p>",
+    '<C-p>',
     function()
-      require('fzf-lua').files({ resume = false })
+      require('fzf-lua').files { resume = false }
     end,
-    desc = "Open file picker",
+    desc = 'Open file picker',
   },
 
   {
-    "<C-S-p>",
+    '<C-S-p>',
     function()
-      require('fzf-lua').files({ resume = true })
+      require('fzf-lua').files { resume = true }
     end,
-    desc = "Open file picker",
+    desc = 'Open file picker',
   },
 
   {
-    "<C-f>",
+    '<C-f>',
     function()
-      require('fzf-lua').live_grep_native({ resume = false })
+      require('fzf-lua').live_grep_native { resume = false }
     end,
-    desc = "Open search in files",
+    desc = 'Open search in files',
   },
 
   {
-    "<C-S-f>",
+    '<C-S-f>',
     function()
-      require('fzf-lua').live_grep_native({ resume = true })
+      require('fzf-lua').live_grep_native { resume = true }
     end,
-    desc = "Open search in files",
+    desc = 'Open search in files',
   },
 
   {
-    "<leader>p",
+    '<leader>p',
     function()
-      require("fzf-lua").global()
+      require('fzf-lua').global()
     end,
-    desc = "Open global picker"
+    desc = 'Open global picker',
   },
 
   {
-    "<leader>e",
+    '<leader>e',
     function()
-      vim.cmd("Neotree toggle")
+      vim.cmd 'Neotree toggle'
     end,
-    desc = "Toggle neotree"
+    desc = 'Toggle neotree',
   },
 
   -- LSP
   {
-    "ö",
+    'ö',
     function()
-      require("conform").format({ lsp_fallback = true })
+      require("conform").format { lsp_fallback = true }
     end,
-    desc = "Format Code"
+    desc = 'Format Code',
   },
 
   {
-    "gd",
+    'gd',
     function()
       vim.lsp.buf.definition()
     end,
-    desc = "Go to definition"
+    desc = 'Go to definition',
   },
 
   {
-    "gr",
+    'gr',
     function()
       vim.lsp.buf.references()
     end,
-    desc = "Go to references"
+    desc = 'Go to references',
   },
 
   {
-    "rn",
+    'rn',
     function()
       vim.lsp.buf.rename()
     end,
-    desc = "Rename symbol under cursor"
+    desc = 'Rename symbol under cursor',
   },
 
   {
-    "<C-Enter>",
+    '<C-Enter>',
     function()
-      require("fzf-lua").lsp_code_actions()
+      require('fzf-lua').lsp_code_actions()
     end,
-    desc = "Open code actions"
-
-  }
-
-
-})
+    desc = 'Open code actions',
+  },
+}
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,22 +1,19 @@
 return {
   'stevearc/conform.nvim',
-  keys = { "ö" },
-  cmd = {
-    "Conform",
-    "ConformInfo",
-    "ConformEnable",
-    "ConformDisable",
-    "ConformToggle",
-  },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { "prettierd", "pretier", stop_after_first = true },
-      typescript = { "prettierd", "prettier", stop_after_first = true },
-      css = { "prettierd", "prettier", stop_after_first = true },
-      html = { "prettierd", "prettier", stop_after_first = true },
-      svelte = { "prettierd", "prettier", stop_after_first = true },
-      astro = { "prettierd", "prettier", stop_after_first = true },
+  event = "LspAttach",
+  config = function()
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        svelte = { 'prettierd', 'prettier', stop_after_first = true },
+        astro = { 'prettierd', 'prettier', stop_after_first = true },
+      },
     }
-  },
+
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
