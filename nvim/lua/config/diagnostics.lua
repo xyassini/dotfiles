15b6d098f886f884251e50df7a465e5894252115
Yassine Zeriouh <y@zeriouh.io>
2025-07-20 17:56:21 +0200

    Adjust diagnostics config

--- nvim/lua/config/diagnostics.lua
+++ nvim/lua/config/diagnostics.lua
@@ -1,9 +1,23 @@
-vim.diagnostic.config({
-  virtual_text = true,
+vim.diagnostic.config {
+  virtual_text = {
+    spacing = 1,
+    severity_limit = 'Warning',
+  },
+  underline = true,
+  update_in_insert = true,
+  severity_sort = true,
+  float = {
+    source = true,
+    focusable = false,
+    severity_sort = true,
+    border = 'rounded',
+  },
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = 'x',
       [vim.diagnostic.severity.WARN] = '!',
+      [vim.diagnostic.severity.INFO] = 'i',
+      [vim.diagnostic.severity.HINT] = '?',
     },
     linehl = {
       [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
@@ -12,4 +26,4 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.WARN] = 'WarningMsg',
     },
   },
-})
+}
