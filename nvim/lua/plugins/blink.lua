d97c3cec690b4f56310996d846e38cf52b14cc67
Yassine Zeriouh <y@zeriouh.io>
2025-07-23 00:05:56 +0200

    Optimize blink config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,71 +1,103 @@
 return {
-  "saghen/blink.cmp",
-  event = "InsertEnter",
-  version = "1.*",
+  'saghen/blink.cmp',
+  event = 'InsertEnter',
+  version = '1.*',
   dependencies = {
     {
-      "fang2hou/blink-copilot",
+      'fang2hou/blink-copilot',
       opts = {
-        debounce = 100
-      }
-    }
+        debounce = 100,
+      },
+    },
   },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     sources = {
-      default = { "lazydev", "lsp", "copilot", "path", "snippets" },
+      default = { 'lazydev', 'lsp', 'copilot', 'path', 'snippets' },
       providers = {
+        lsp = {
+          score_offset = 1,
+        },
         lazydev = {
-          name = "LazyDev",
-          module = "lazydev.integrations.blink",
-          -- make lazydev completions top priority (see `:h blink.cmp`)
+          name = 'LazyDev',
+          module = 'lazydev.integrations.blink',
           score_offset = 100,
         },
         copilot = {
-          name = "copilot",
-          module = "blink-copilot",
-          score_offset = -10,
+          name = 'copilot',
+          module = 'blink-copilot',
+          score_offset = -1,
           async = true,
         },
       },
     },
-    keymap = {
-      preset = "default",
-
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide", "fallback" },
-
-      ["<Tab>"] = { "accept", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    fuzzy = {
+      sorts = {
+        'exact',
+        'score',
+        'sort_text',
+      },
     },
-    signature = { enabled = true },
+    keymap = {
+      preset = 'default',
+
+      ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+      ['<C-e>'] = { 'hide', 'fallback' },
+
+      ['<Tab>'] = { 'accept', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+      ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+      ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+    },
+    signature = {
+      enabled = true,
+      trigger = {
+        enabled = true,
+        show_on_keyword = true,
+        blocked_trigger_characters = {},
+        blocked_retrigger_characters = {},
+        show_on_trigger_character = true,
+        show_on_insert = true,
+        show_on_insert_on_trigger_character = true,
+      },
+    },
     cmdline = {
       enabled = true,
       completion = { menu = { auto_show = true } },
     },
     completion = {
-      prefetch_on_insert = true,
-      show_on_backspace = true,
-      show_on_backspace_in_keyword = true,
-      show_on_insert = true,
+      keyword = {
+        range = 'prefix',
+      },
+      trigger = {
+        prefetch_on_insert = true,
+        show_on_backspace = true,
+        show_on_backspace_in_keyword = true,
+        show_on_insert = true,
+        show_on_blocked_trigger_characters = {},
+        show_on_x_blocked_trigger_characters = {},
+        show_on_y_blocked_trigger_characters = {},
+      },
       documentation = {
         auto_show = true,
-        auto_show_delay_ms = 200
+        auto_show_delay_ms = 200,
       },
       menu = {
-        auto_show = true
-      }
-    }
+        auto_show = true,
+      },
+      list = {
+        preselect = true,
+        auto_insert = false,
+      },
+    },
   },
 }
