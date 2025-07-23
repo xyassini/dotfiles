c3831bb281417b345bda25bb45d131cfdb0b54d9
Yassine Zeriouh <y@zeriouh.io>
2025-07-24 01:56:56 +0200

    Fix autosave

--- nvim/lua/custom/auto-save.lua
+++ nvim/lua/custom/auto-save.lua
@@ -1,12 +1,12 @@
 local function clear_cmdarea()
   vim.defer_fn(function()
     vim.api.nvim_echo({}, false, {})
-  end, 800)
+  end, 1000)
 end
 
 vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, {
   callback = function()
-    if vim.buftype and #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
+    if vim.bo.buftype == '' and #vim.api.nvim_buf_get_name(0) ~= 0 and vim.bo.buflisted then
       vim.cmd 'silent w'
 
       local time = os.date '%I:%M %p'
