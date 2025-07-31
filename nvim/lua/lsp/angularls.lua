bb4912e5de32e448590ad3d06cf551959d860c07
Yassine Zeriouh <y@zeriouh.io>
2025-08-01 01:20:26 +0200

    Fix typescript lsp

--- nvim/lua/lsp/angularls.lua
+++
@@ -1,5 +0,0 @@
-vim.lsp.config('angularls', {
-  filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx', 'htmlangular' }
-})
-
-vim.cmd("TSBufEnable highlight")
--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -14,14 +14,4 @@ return {
     'nvim-lua/plenary.nvim',
     'neovim/nvim-lspconfig',
   },
-  opts = {
-    expose_as_code_action = 'all',
-    complete_function_calls = true,
-    include_completions_with_insert_text = true,
-  },
-  config = function(_, opts)
-    -- vim.lsp.enable('ts_ls', false)
-    -- require('typescript-tools').setup(opts)
-
-  end,
 }
