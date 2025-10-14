4e5c96452cb133ceadb1554ec76d858d4abceffb
Yassine Zeriouh <y@zeriouh.io>
2025-10-15 01:20:38 +0200

    Add flash keybinds

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
   'folke/flash.nvim',
   ---@type require('flash').Config
@@ -10,6 +11,38 @@ return {
         require('flash').jump()
       end,
       desc = 'Flash',
+    },
+    {
+      'S',
+      mode = { 'n', 'x', 'o' },
+      function()
+        require('flash').treesitter()
+      end,
+      desc = 'Flash Treesitter',
+    },
+    {
+      'r',
+      mode = 'o',
+      function()
+        require('flash').remote()
+      end,
+      desc = 'Remote Flash',
+    },
+    {
+      'R',
+      mode = { 'o', 'x' },
+      function()
+        require('flash').treesitter_search()
+      end,
+      desc = 'Treesitter Search',
+    },
+    {
+      '<c-s>',
+      mode = { 'c' },
+      function()
+        require('flash').toggle()
+      end,
+      desc = 'Toggle Flash Search',
     },
   },
 }
