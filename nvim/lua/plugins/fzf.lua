66668c9a2d45b08889be2096a53c638f305aa08d
Yassine Zeriouh <y@zeriouh.io>
2025-10-15 01:20:31 +0200

    fix fzf setup

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,13 +1,15 @@
 return {
-  "ibhagwan/fzf-lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  cmd = { "FzfLua" },
+  'ibhagwan/fzf-lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = 'VeryLazy',
+  cmd = { 'FzfLua' },
   ---@module 'fzf-lua'
   ---@type fzf-lua.Config
   opts = {
-    "max-perf"
+    'max-perf',
   },
-  config = function ()
-    require("fzf-lua").register_ui_select()
-  end
+  config = function(opts)
+    require('fzf-lua').setup(opts)
+    require('fzf-lua').register_ui_select()
+  end,
 }
