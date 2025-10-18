141788a7a61faaec3d5e08ef8b7e5a7d887d33bb
Yassine Zeriouh <y@zeriouh.io>
2025-10-18 13:23:37 +0200

    Remove unmaintained typescript-tools

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,11 +1,1 @@
 vim.lsp.enable 'sourcekit'
-
-vim.lsp.enable('ts_ls', false)
-require('typescript-tools').setup {
-  expose_as_code_action = 'all',
-  complete_function_calls = true,
-  include_completions_with_insert_text = true,
-}
-
-
-
